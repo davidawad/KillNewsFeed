ae436f8b733346534ff9b3bfba29cf7471559f85
David Awad <davidawad64@gmail.com>
2017-06-15 10:20:08 -0400

    FEAT: returning functionality to this extension

--- kill.js
+++ kill.js
@@ -1,26 +1,30 @@
 function blockAndDisplay() {
     var feed = $('[id^=topnews_main_stream], [id^=mostrecent_main_stream], [id^=pagelet_home_stream]');
-    var message = $('#distracted');
+    var message = $('#ContentArea');
 
-    if (feed.length == 0) {
-        message.remove();
-    } else if (message.length == 0) {
-        message = $('<h1>')
-            .attr('id', 'distracted')
-            .text("Don't get distracted by Facebook!")
-            .css('font-size', '30px')
-            .css('font-family', "'Helvetica Neue', Helvetica, Arial, 'lucida grande', tahoma, verdana, arial, sans-serif")
-            .css('position', 'relative')
-            .css('top', '75px');
-        $('[data-location=maincolumn]').append(message);
+    // use our tag to check if we've run
+    done = $('#distracted').length;
+
+    // if the distracted tag exists, a length of 1 will be returned
+    if ( !done ) {
+      message.remove();
+
+      message = $('<h1>')
+          .attr('id', 'distracted')
+          .text("Don't get distracted by Facebook!")
+          .css('font-size', '30px')
+          .css('font-family', "'Helvetica Neue', Helvetica, Arial, 'lucida grande', tahoma, verdana, arial, sans-serif")
+          .css('position', 'relative')
+          .css('top', '75px');
+      $('#contentArea').append(message);
+
+      feed.children().remove();
+      $('.ticker_stream').remove();
+      $('.ego_column').remove();
+      $('#pagelet_games_rhc').remove();
+      $('#pagelet_trending_tags_and_topics').remove();
+      $('#pagelet_canvas_nav_content').remove();
      }
-
-    feed.children().remove();
-    $('.ticker_stream').remove();
-    $('.ego_column').remove();
-    $('#pagelet_games_rhc').remove();
-    $('#pagelet_trending_tags_and_topics').remove();
-    $('#pagelet_canvas_nav_content').remove();
 }
 
 window.setInterval(blockAndDisplay, 100);
